83792ebb12c9b0b3d75218ebb7eb93705f0a5326
bp456@hotmail.com <bp456@hotmail.com>
2015-02-21 11:43:49 -0600

    Converted collection view to angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,8 +3,33 @@
 //require('./album');
 //require("./profile");
 
-blocJams = angular.module('BlocJams', ['ui.router']);
+// Example album.
+var albumPicasso = {
+    name: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: '/images/album-placeholder.png',
 
+    songs: [{
+        name: 'Blue',
+        length: '4:26'
+    }, {
+        name: 'Green',
+        length: '3:14'
+    }, {
+        name: 'Red',
+        length: '5:01'
+    }, {
+        name: 'Pink',
+        length: '3:21'
+    }, {
+        name: 'Magenta',
+        length: '2:15'
+    }]
+};
+
+blocJams = angular.module('BlocJams', ['ui.router']);
 blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
 
@@ -13,10 +38,18 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
         controller: 'Landing.controller',
         templateUrl: '/templates/landing.html'
     });
+
+    $stateProvider.state('collection', {
+        url: '/collection',
+        controller: 'Collection.controller',
+        templateUrl: '/templates/collection.html'
+    });
 }]);
 
 // This is a cleaner way to call the controller than crowding it on the module definition.
 blocJams.controller('Landing.controller', ['$scope', function($scope) {
+    $scope.subText = "Turn the music up!";
+
     $scope.subTextClicked = function() {
         $scope.subText += '!';
     };
@@ -32,4 +65,11 @@ blocJams.controller('Landing.controller', ['$scope', function($scope) {
         '/images/album-placeholders/album-8.jpg',
         '/images/album-placeholders/album-9.jpg',
     ];
+}]);
+
+blocJams.controller('Collection.controller', ['$scope', function($scope) {
+    $scope.albums = [];
+    for (var i = 0; i < 33; i++) {
+        $scope.albums.push(angular.copy(albumPicasso));
+    }
 }]);
