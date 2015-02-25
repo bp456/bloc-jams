4fb0fe4880c178581c754736207660902313ca0c
bp456@hotmail.com <bp456@hotmail.com>
2015-02-25 15:12:03 -0600

    Added a time indicator for song progress

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -126,9 +126,16 @@ blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope
 
 blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
     $scope.songPlayer = SongPlayer;
+
+    SongPlayer.onTimeUpdate(function(event, time) {
+        $scope.$apply(function() {
+            $scope.playTime = time;
+        });
+    });
+
 }]);
 
-blocJams.service('SongPlayer', function() {
+blocJams.service('SongPlayer', ['$rootScope', function($rootScope) {
     var currentSoundFile = null;
     var trackIndex = function(album, song) {
         return album.songs.indexOf(song);
@@ -174,21 +181,31 @@ blocJams.service('SongPlayer', function() {
                 currentSoundFile.setTime(time);
             }
         },
+
+        onTimeUpdate: function(callback) {
+            return $rootScope.$on('sound:timeupdate', callback);
+        },
+
         setSong: function(album, song) {
             if (currentSoundFile) {
                 currentSoundFile.stop();
             }
             this.currentAlbum = album;
             this.currentSong = song;
+
             currentSoundFile = new buzz.sound(song.audioUrl, {
                 formats: ["mp3"],
                 preload: true
             });
 
+            currentSoundFile.bind('timeupdate', function(e) {
+                $rootScope.$broadcast('sound:timeupdate', this.getTime());
+            });
+
             this.play();
         }
     };
-});
+}]);
 
 blocJams.directive('slider', ['$document', function($document) {
 
@@ -270,7 +287,7 @@ blocJams.directive('slider', ['$document', function($document) {
             scope.onClickSlider = function(event) {
                 var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
                 scope.value = percent * scope.max;
-                  notifyCallback(scope.value);
+                notifyCallback(scope.value);
 
             };
 
@@ -279,7 +296,7 @@ blocJams.directive('slider', ['$document', function($document) {
                     var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
                     scope.$apply(function() {
                         scope.value = percent * scope.max;
-                          notifyCallback(scope.value);
+                        notifyCallback(scope.value);
 
                     });
                 });
@@ -295,3 +312,32 @@ blocJams.directive('slider', ['$document', function($document) {
         }
     };
 }]);
+
+blocJams.filter('timecode', function() {
+    return function(seconds) {
+        seconds = Number.parseFloat(seconds);
+
+        // Returned when no time is provided.
+        if (Number.isNaN(seconds)) {
+            return '-:--';
+        }
+
+        // make it a whole number
+        var wholeSeconds = Math.floor(seconds);
+
+        var minutes = Math.floor(wholeSeconds / 60);
+
+        remainingSeconds = wholeSeconds % 60;
+
+        var output = minutes + ':';
+
+        // zero pad seconds, so 9 seconds should be :09
+        if (remainingSeconds < 10) {
+            output += '0';
+        }
+
+        output += remainingSeconds;
+
+        return output;
+    };
+});
